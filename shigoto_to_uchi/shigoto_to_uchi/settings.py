89c7e4268d5d7124099feb77a9a0a5e5a78cfe80
claudym <claudy.m.mejia@gmail.com>
2023-01-31 11:50:57 -0400

    disable browsable api

--- shigoto_to_uchi/shigoto_to_uchi/settings.py
+++ shigoto_to_uchi/shigoto_to_uchi/settings.py
@@ -124,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# THIS DISABLES THE BROWSABLE API
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
